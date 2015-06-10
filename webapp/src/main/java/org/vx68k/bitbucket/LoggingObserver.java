432055bfe817b4e25a9c29d1a8dffba3a13408cd
Kaz Nishimura <kazssym@vx68k.org>
2015-06-10 20:46:49 +0900

    Added an example web application.

---
+++ webapp/src/main/java/org/vx68k/bitbucket/LoggingObserver.java
@@ -0,0 +1,40 @@
+/*
+ * LoggingObserver
+ * Copyright (C) 2015 Kaz Nishimura
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.vx68k.bitbucket;
+
+import java.util.logging.Logger;
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.event.Observes;
+import org.vx68k.bitbucket.servlet.BitbucketPushNotification;
+
+/**
+ * Logs the JSON object for each push notification.
+ *
+ * @author Kaz Nishimura
+ * @since 1.0
+ */
+@ApplicationScoped
+public class LoggingObserver {
+
+    private final Logger logger = Logger.getLogger(getClass().getName());
+
+    public void commitNotified(@Observes BitbucketPushNotification push) {
+        logger.info(push.getJson().toString());
+    }
+}
---
+++ webapp/src/main/java/org/vx68k/bitbucket/hook/app/AppicationConfig.java
@@ -0,0 +1,42 @@
+/*
+ * ApplicationConfig - application configuration
+ * Copyright (C) 2015 Kaz Nishimura
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.vx68k.bitbucket.hook.app;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import org.vx68k.bitbucket.api.util.Credentials;
+
+/**
+ * Application configuration.
+ *
+ * @author Kaz Nishimura
+ * @since 1.0
+ */
+@ApplicationScoped
+@Named
+public class AppicationConfig {
+
+    @Inject
+    private Credentials clientCredentials;
+
+    public Credentials getClientCredentials() {
+        return clientCredentials;
+    }
+}
