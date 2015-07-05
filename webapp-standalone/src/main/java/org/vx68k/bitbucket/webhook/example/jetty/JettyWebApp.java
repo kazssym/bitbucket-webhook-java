0513c3da420c1286d3ae7cbb5fb0e8ad16a62751
Kaz Nishimura <kazssym@vx68k.org>
2015-07-05 20:48:42 +0900

    Changed the standalone server to use bitbucket-webhook-example-servlet and to work as expected.

--- webapp-standalone/src/main/java/org/vx68k/bitbucket/webhook/example/jetty/JettyWebApp.java
+++ webapp-standalone/src/main/java/org/vx68k/bitbucket/webhook/example/jetty/JettyWebApp.java
@@ -18,10 +18,20 @@
 
 package org.vx68k.bitbucket.webhook.example.jetty;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Properties;
+import org.eclipse.jetty.deploy.App;
+import org.eclipse.jetty.deploy.AppProvider;
+import org.eclipse.jetty.deploy.DeploymentManager;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.ContextHandler;
+import org.eclipse.jetty.server.handler.ContextHandlerCollection;
+import org.eclipse.jetty.util.component.AbstractLifeCycle;
 import org.eclipse.jetty.webapp.Configuration;
 import org.eclipse.jetty.webapp.WebAppContext;
-
+import org.eclipse.jetty.webapp.WebInfConfiguration;
 
 /**
  * Web application main class for Jetty.
@@ -29,9 +39,7 @@ import org.eclipse.jetty.webapp.WebAppContext;
  * @author Kaz Nishimura
  * @since 1.0
  */
-public class JettyWebApp {
-
-    private static final int DEFAULT_HTTP_PORT = 8080;
+public class JettyWebApp extends AbstractLifeCycle implements AppProvider {
 
     /**
      * Name of the property name for the HTTP port.
@@ -39,12 +47,26 @@ public class JettyWebApp {
     public static final String HTTP_PORT_NAME
             = "org.vx68k.bitbucket.webhook.example.httpPort";
 
+    private static final int DEFAULT_HTTP_PORT = 8080;
+
+    private static final String SERVER_PROPERTIES
+            = "META-INF/server.properties";
+
+    private DeploymentManager deploymentManager;
+
+    private App rootApp;
+
     /**
      * Runs a webhook example application.
      *
      * @exception Exception if any exception occurs
      */
     protected void run() throws Exception {
+        Properties serverProperties = new Properties();
+        serverProperties.load(
+                ClassLoader.getSystemResourceAsStream(SERVER_PROPERTIES));
+        System.getProperties().putAll(serverProperties);
+
         int httpPort = DEFAULT_HTTP_PORT;
         String httpPortProperty = System.getProperty(HTTP_PORT_NAME);
         if (httpPortProperty != null) {
@@ -52,19 +74,24 @@ public class JettyWebApp {
         }
 
         Server server = new Server(httpPort);
+
+        // Setting up for jetty-annotations.
         Configuration.ClassList classes
                 = Configuration.ClassList.setServerDefault(server);
         classes.addBefore(
                 "org.eclipse.jetty.webapp.JettyWebXmlConfiguration",
                 "org.eclipse.jetty.annotations.AnnotationConfiguration");
 
-        WebAppContext context = new WebAppContext();
-        context.setParentLoaderPriority(true);
-        context.setWar(ClassLoader.getSystemResource("META-INF/webapp").toString());
-        context.setAttribute(
-                "org.eclipse.jetty.server.webapp.ContainerIncludeJarPattern",
-                ".*/.*\\.jar$");
-        server.setHandler(context);
+        ContextHandlerCollection contexts = new ContextHandlerCollection();
+        server.setHandler(contexts);
+
+        DeploymentManager deploymentManager = new DeploymentManager();
+        deploymentManager.setContexts(contexts);
+        deploymentManager.setContextAttribute(
+                WebInfConfiguration.CONTAINER_JAR_PATTERN,
+                ".*\\.jar$|.*/classes/?$");
+        deploymentManager.addAppProvider(this);
+        server.addBean(deploymentManager);
 
         server.start();
         server.join();
@@ -81,5 +108,56 @@ public class JettyWebApp {
         } catch (Exception e) {
             System.err.println(e);
         }
+    }
+
+    @Override
+    protected void doStart() {
+        if (rootApp != null) {
+            throw new IllegalStateException("App is already created");
+        }
+
+        try {
+            rootApp = new App(deploymentManager, this, "META-INF/webapp");
+            deploymentManager.addApp(rootApp);
+        } catch (RuntimeException e) {
+            rootApp = null;
+            throw e;
+        }
+    }
+
+    @Override
+    protected void doStop() {
+        if (rootApp != null) {
+            try {
+                deploymentManager.removeApp(rootApp);
+            } finally {
+                rootApp = null;
+            }
+        }
+    }
+
+    @Override
+    public void setDeploymentManager(DeploymentManager deploymentManager) {
+        this.deploymentManager = deploymentManager;
+    }
+
+    @Override
+    public ContextHandler createContextHandler(App app) throws IOException {
+        URL appResource = ClassLoader.getSystemResource(app.getOriginId());
+        if (appResource == null) {
+            throw new FileNotFoundException("Web application not found");
+        }
+
+        WebAppContext contextHandler = new WebAppContext();
+        // Excluding some from the server classes for Weld to work.
+        contextHandler.prependServerClass(
+                "-org.eclipse.jetty.server.handler.ContextHandler");
+        contextHandler.prependServerClass(
+                "-org.eclipse.jetty.servlet.ServletContextHandler");
+        contextHandler.prependServerClass(
+                "-org.eclipse.jetty.servlet.ServletHandler");
+
+        contextHandler.setWar(appResource.toString());
+        return contextHandler;
     }
 }
