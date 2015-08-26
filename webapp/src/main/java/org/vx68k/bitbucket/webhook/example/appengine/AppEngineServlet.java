789f0448deee5266cf3277497579429fa44abc8a
Kaz Nishimura <kazssym@vx68k.org>
2015-08-26 14:01:48 +0900

    Moved a class.

--- webapp/src/main/java/org/vx68k/bitbucket/webhook/example/appengine/AppEngineServlet.java
+++ webapp/src/main/java/org/vx68k/bitbucket/webhook/example/appengine/AppEngineServlet.java
@@ -16,7 +16,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package org.vx68k.bitbucket.webhook.example.servlet;
+package org.vx68k.bitbucket.webhook.example.appengine;
 
 import java.io.IOException;
 import java.io.PrintWriter;
