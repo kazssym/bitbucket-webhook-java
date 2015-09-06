859a830e0d6b1d5434e508660a1291db12c600b7
Kaz Nishimura <kazssym@vx68k.org>
2015-09-06 17:01:02 +0900

    Implemented a drop-down menu behavior.

--- example/src/main/webapp/resources/scripts/example.js
+++ example/src/main/webapp/resources/scripts/example.js
@@ -17,3 +17,21 @@
 */
 
 "use strict";
+
+$(document).ready(function () {
+    var accountMenu = $("#example-account-menu");
+    accountMenu.removeClass("pure-menu-allow-hover");
+    accountMenu.mouseleave(function (event) {
+        $("#example-account-menu-children").css("display", "");
+    });
+
+    $("#example-account-menu-link").click(function (event) {
+        var children = $("#example-account-menu-children");
+        if (children.css("display") !== "block") {
+            children.css("display", "block");
+        } else {
+            accountMenu.mouseleave();
+        }
+        return false;
+    });
+});
